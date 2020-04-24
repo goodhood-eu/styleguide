6b04a70581a450531bbcbd0d4e026be59b1ea673
Max Degterev <max@degterev.me>
2020-04-24 17:00:25 +0200

    fxi

--- javascript/packages/eslint-config-nebenan-base/test/valid/modules.js
+++ javascript/packages/eslint-config-nebenan-base/test/valid/modules.js
@@ -4,6 +4,8 @@ import 'eslint';
 import alias from '@/packages/eslint-config-nebenan-base/package-lock';
 import pkg from '../../package';
 
+console.warn('Used dep', alias, pkg);
+
 const requireInFunc = () => require('path');
 
 const dynamicRequire = () => require(`${'path'}`);
